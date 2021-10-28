b90b4aa5e8c80079c0db12ccfe378e9a1dbe9d47
Ezequiel2387 <91636040+Ezequiel2387@users.noreply.github.com>
2021-10-28 13:59:19 -0300

    Add files via upload

---
+++ Desafio 5 con Ejs/routes/rutas.js
@@ -0,0 +1,50 @@
+const express=require("express")
+const app=express()
+const {Router}=express
+const router=new Router()
+const arr=require("../data/index")
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+router.get("/producto",(req,res)=>{                                     //Veo todos los productos
+    if(arr.length==0){
+        res.render("vacio.ejs")
+    }else{
+        res.render("productoslistados",{data:arr})
+    }   
+})
+router.get("/productos",(req,res)=>{                                    //Renderizo Formulario
+    res.render("productos",{})
+})
+router.get("/productos/:id",(req,res)=>{                                //Busco producto por /:id
+    let arrnew=arr.filter((x)=> x.id==req.params.id )
+    arrnew.length==0?res.send({ error : 'producto no encontrado'}):res.send(arrnew)
+})
+router.post("/productos",(req,res)=>{                                   //Cargo productos
+    let {titulo,precio,url}=req.body
+    let obj={
+        titulo,
+        precio,
+        url
+    }
+    obj.id=arr.length+1
+    arr.push(obj)
+    res.redirect("/productos")
+    //res.send(`El producto ingresado posee un id de ${obj.id}`)
+})
+router.put("/productos/:id",(req,res)=>{                                //Actualizo un producto segun su /:id
+    let {titulo,precio,url}=req.body
+    let obj={
+        titulo,
+        precio,
+        url
+        } 
+    obj.id=req.params.id
+    arr[req.params.id-1]=obj
+    res.send("Producto actualizado correctamente")  
+ })
+router.delete("/productos/:id",(req,res)=>{                             //Borro un producto segun /:id
+    arr.splice(req.params.id-1,1)
+    arr.length==0?res.send("No hay productos para borrar"):res.send(`Producto borrado cuyo id es = ${req.params.id}`)
+})
+module.exports=router
