e859d279a5b7152edb537ee0f9ecdb8379c134c2
Ezequiel2387 <91636040+Ezequiel2387@users.noreply.github.com>
2022-01-01 12:31:23 -0300

    Add files via upload

---
+++ Desafio Normalizr Rodriguez Ezequiel/index.js
@@ -0,0 +1,53 @@
+const express=require("express");
+const app=express();
+const Port=process.env.Port || 8080                 //Puerto
+const rutas=require("./routes/rutas");
+const handlebars=require("express-handlebars");
+const util=require("util");
+const mongoose=require("mongoose")
+const http=require("http");                          //Servidor HTTP
+const server=http.createServer(app);                 //
+const {normalize,schema,denormalize}=require("normalizr");
+require("./db");                                    //MongoDB
+const mensajesmongodb=require("./models/Mensajes")  //MongoDB
+      
+
+app.use(express.static(__dirname+"/public"))        //Usamos archivos estaticos
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.engine("hbs",handlebars({
+    layoutsDir:__dirname+"/views/layouts"
+}))
+app.set("views",__dirname+"/views")
+app.set("view engine", "hbs")
+
+const msn=[]
+app.use("/",rutas)                                  //Rutas
+
+const {Server}=require("socket.io");                 //Servidor de socket
+const { Console } = require("console");
+const io=new Server(server)              
+
+
+io.on("connection",(socket)=>{                      //Servidor escuchando un evento
+    socket.emit("message_back",msn)
+    socket.on("message_client",(data)=>{
+        console.log(data)
+    })
+    socket.on("data_client",(data)=>{
+        msn.push(data)
+        console.log(data)
+        const msj = new mensajesmongodb(data);
+        msj.save((err,document)=>{
+          if(err) console.log(err)
+          console.log(document)
+        })
+        io.sockets.emit("message_back",msn)
+    })
+})
+
+server.listen(Port,()=>{
+    console.log(`Port is running on ${Port}`)
+})
